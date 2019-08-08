42b9b74c5caef9c6ae3fbedb6f3bf4acd155058f
Paul Chavard <git@paul.chavard.net>
2019-08-08 10:29:03 +0100

    Extract from orbit-server

---
+++ test/sql-source-test.ts
@@ -0,0 +1,39 @@
+import { Schema } from '@orbit/data';
+
+import SQLSource from '../src';
+
+QUnit.module('sql source', function(hooks) {
+  let schema: Schema;
+  let source: SQLSource;
+
+  hooks.beforeEach(async function() {
+    schema = new Schema({
+      models: {
+        user: {
+          attributes: {
+            name: { type: 'string' }
+          }
+        }
+      }
+    });
+
+    source = new SQLSource({
+      schema,
+      knex: {
+        client: 'sqlite3',
+        connection: { filename: ':memory:' },
+        useNullAsDefault: true
+      }
+    });
+    await source.activated;
+  });
+
+  hooks.beforeEach(async function() {
+    await source.deactivate();
+  });
+
+  QUnit.test('it exists', function(assert) {
+    assert.ok(source);
+    assert.ok(source instanceof SQLSource);
+  });
+});
