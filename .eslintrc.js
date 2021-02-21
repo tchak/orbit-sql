147399bfec368242139d5fd26cc0c92cb123daed
Paul Chavard <github@paul.chavard.net>
2021-02-21 18:47:36 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,15 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
+    ecmaVersion: 2020,
+    sourceType: 'module',
   },
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   env: {
-    node: true
+    node: true,
   },
   rules: {
     '@typescript-eslint/no-empty-interface': ['off'],
@@ -20,7 +19,8 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': ['off'],
     '@typescript-eslint/explicit-member-accessibility': ['off'],
     '@typescript-eslint/no-empty-function': ['off'],
+    '@typescript-eslint/explicit-module-boundary-types': ['off'],
     'prefer-const': ['off'],
-    'prefer-rest-params': ['off']
-  }
+    'prefer-rest-params': ['off'],
+  },
 };
